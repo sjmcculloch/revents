75df2c66e995899e1c8593839bdb66311f4fd007
Scott McCulloch <smcculloch@gmail.com>
2018-07-20 10:43:46 +1000

    End of Section 3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
 import registerServiceWorker from './registerServiceWorker';
@@ -11,7 +12,7 @@ let render = () => {
 }
 
 if (module.hot) {
-    module.hot.accept('./App', () => {
+    module.hot.accept('./app/layout/App', () => {
         setTimeout(render);
     });
 }
