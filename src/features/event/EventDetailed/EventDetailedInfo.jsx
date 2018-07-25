87dca8b34276fb05a24ca98ae4f581da840dbf26
Scott McCulloch <smcculloch@gmail.com>
2018-07-25 22:13:25 +1000

    End of Section 19

--- src/features/event/EventDetailed/EventDetailedInfo.jsx
+++ src/features/event/EventDetailed/EventDetailedInfo.jsx
@@ -22,6 +22,10 @@ class EventDetailedInfo extends Component {
 
   render() {
     const { event } = this.props;
+    let eventDate;
+    if (event.date) {
+      eventDate = event.date.toDate();
+    }
     return (
       <Segment.Group>
         <Segment attached="top">
@@ -41,8 +45,8 @@ class EventDetailedInfo extends Component {
             </Grid.Column>
             <Grid.Column width={15}>
               <span>
-                {format(event.date, "dddd Do MMM")} at{" "}
-                {format(event.date, "HH:mm")}
+                {format(eventDate, "dddd Do MMM")} at{" "}
+                {format(eventDate, "HH:mm")}
               </span>
             </Grid.Column>
           </Grid>
