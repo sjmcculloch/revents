9aef835d61d981355090cce4930e4b5e343bcb17
Scott McCulloch <smcculloch@gmail.com>
2018-07-31 22:31:21 +1000

    End of Section 20

--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -1,11 +1,13 @@
 import moment from "moment";
 import cuid from "cuid";
 import { toastr } from "react-redux-toastr";
+import { FETCH_EVENTS } from "../event/eventConstants";
 import {
   asyncActionStart,
   asyncActionFinish,
   asyncActionError
 } from "../async/asyncActions";
+import firebase from "../../app/config/firebase";
 
 export const updateProfile = user => async (
   dispatch,
@@ -162,5 +164,59 @@ export const cancelGoingToEvent = event => async (
   } catch (error) {
     console.log(error);
     toastr.error("Oops", "Something went wrong");
+  }
+};
+
+export const getUserEvents = (userUid, activeTab) => async (
+  dispatch,
+  getState
+) => {
+  dispatch(asyncActionStart());
+  const firestore = firebase.firestore();
+  const today = new Date(Date.now());
+  let eventsRef = firestore.collection("event_attendee");
+  let query;
+  switch (activeTab) {
+    case 1: // past events
+      query = eventsRef
+        .where("userUid", "==", userUid)
+        .where("eventDate", "<=", today)
+        .orderBy("eventDate", "desc");
+      break;
+    case 2: // future events
+      query = eventsRef
+        .where("userUid", "==", userUid)
+        .where("eventDate", ">=", today)
+        .orderBy("eventDate");
+      break;
+    case 3: // hosted events
+      query = eventsRef
+        .where("userUid", "==", userUid)
+        .where("host", "==", true)
+        .orderBy("eventDate", "desc");
+      break;
+    default:
+      query = eventsRef
+        .where("userUid", "==", userUid)
+        .orderBy("eventDate", "desc");
+  }
+  try {
+    let querySnap = await query.get();
+    let events = [];
+
+    for (let i = 0; i < querySnap.docs.length; i++) {
+      let evt = await firestore
+        .collection("events")
+        .doc(querySnap.docs[i].data().eventId)
+        .get();
+      events.push({ ...evt.data(), id: evt.id });
+    }
+    dispatch({ type: FETCH_EVENTS, payload: { events } });
+
+    console.log(querySnap);
+    dispatch(asyncActionFinish());
+  } catch (error) {
+    console.log(error);
+    dispatch(asyncActionError());
   }
 };
